bcac3284992aa3a2b372c96f023a9f300459a386
robbinl <robbinl@telusplanet.net>
2020-07-23 10:38:41 -0600

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
+#define LED_ONBOARD_PIN   2
+#define LED_PIN   25
+#define BTN_PIN   16
+
+struct Led {
+    // state variables
+    uint8_t pin;
+    bool    on;
+
+    // methods
+    void update() {
+        digitalWrite(pin, on ? HIGH : LOW);
+    }
+};
+
+Led    onboard_led = { LED_ONBOARD_PIN, false };
+Led    led         = { LED_PIN, false };
+
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(onboard_led.pin, OUTPUT);
+    pinMode(led.pin,         OUTPUT);
+
+    Serial.begin(9600); delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    onboard_led.on = millis() % 2000 < 50;
+    led.on = millis() % 1000 < 50;
+
+    led.update();
+    onboard_led.update();
 }
