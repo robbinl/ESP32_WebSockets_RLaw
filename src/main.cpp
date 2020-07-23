8db3987651add934555d93afb447523ac4a3ca71
sunny <sunny.722@hotmail.com>
2020-07-23 14:23:02 -0600

    Revert "a"
    This reverts commit 19973bec94cf308e2b30e88456742ff567247b27.

--- src/main.cpp
+++ src/main.cpp
@@ -55,13 +55,6 @@ struct Button {
     }
 };
 
-// Global Variables
-Led    onboard_led = { LED_ONBOARD_PIN, false };
-Led    led1        = { LED1_PIN, false };
-Button button1      = { BTN1_PIN, HIGH, 0, 0 };
-Led    led2        = { LED2_PIN, false };
-Button button2      = { BTN2_PIN, HIGH, 0, 0 };
-
 // SPIFFS
 void initSPIFFS() {
   if (!SPIFFS.begin()) {
@@ -72,6 +65,13 @@ void initSPIFFS() {
     }
   }
 }
+
+// Global Variables
+Led    onboard_led = { LED_ONBOARD_PIN, false };
+Led    led1        = { LED1_PIN, false };
+Button button1      = { BTN1_PIN, HIGH, 0, 0 };
+Led    led2        = { LED2_PIN, false };
+Button button2      = { BTN2_PIN, HIGH, 0, 0 };
 
 void setup() {
     pinMode(onboard_led.pin,  OUTPUT);
@@ -95,4 +95,6 @@ void loop() {
     else if (button2.released()) led2.on = false;
     led1.update();
     led2.update();
+    onboard_led.on = millis() % 1000 < 50;
+    onboard_led.update();
 }
